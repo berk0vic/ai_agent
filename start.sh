cd7ad08720ac13b4fc5da109be51dbcf09c42fc4
fixstart <berktahir.kilic@std.yeditepe.edu.tr>
2025-08-14 16:35:05 +0300

    Fix Gunicorn startup command,

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Sanal ortamı etkinleştir
-source /home/site/wwwroot/antenv/bin/activate
-# Gunicorn ile uygulamayı başlat
-gunicorn teams_bot:create_app
+. /home/site/wwwroot/antenv/bin/activate
+export PYTHONPATH=$PYTHONPATH:/home/site/wwwroot
+gunicorn agent.teams_bot:create_app
